0f6dde5d06dc22648fe4a755f90ae9d1f09558b1
remittor <remittor@gmail.com>
2021-05-19 06:58:26 +0300

    Fix device admin installation

--- app/src/main/java/app/kidswarmup/SettingsActivity.java
+++ app/src/main/java/app/kidswarmup/SettingsActivity.java
@@ -20,6 +20,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import java.io.DataOutputStream;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import app.kidswarmup.MainActivity;
@@ -86,14 +87,14 @@ public class SettingsActivity extends AppCompatActivity implements
         int id = view.getId();
         if (id == R.id.app_close_button)
             onAppCloseButtonClick();
+        if (id == R.id.devadm_activate_button)
+            onDevAdmActivateButtonClick();
         if (id == R.id.devadm_install_button)
             onDevAdmInstallButtonClick();
         if (id == R.id.devadm_policies_button)
             onDevAdmUpdatePoliciesButtonClick();
-        if (id == R.id.devadm_activate_button)
-            onDevAdmActivateButtonClick();
         if (id == R.id.devadm_delete_button)
-            onDevAdmDeleteButtonClick(false);
+            onDevAdmDeleteButtonClick();
     }
 
     private void onAppCloseButtonClick() {
@@ -114,44 +115,68 @@ public class SettingsActivity extends AppCompatActivity implements
         return false;
     }
 
+    private boolean devAdmDeleteFiles(String flist, boolean silent) {
+        if (!remountDataPart(true, silent))
+            return false;
+        ArrayList<String> cmd = new ArrayList<String>();
+        if (flist.contains("device_owner.xml") || flist.contains("dev_owner"))
+            cmd.add("rm -f /data/system/device_owner.xml");
+        if (flist.contains("device_owner_2.xml") || flist.contains("dev_owner_2"))
+            cmd.add("rm -f /data/system/device_owner_2.xml");
+        if (flist.contains("device_policy_2.xml") || flist.contains("dev_pol_2"))
+            cmd.add("rm -f /data/system/device_policy_2.xml");
+        if (flist.contains("device_policies.xml") || flist.contains("dev_pol"))
+            cmd.add("rm -f /data/system/device_policies.xml");
+        try {
+            doSuCmd(cmd.toArray(new String[0]));
+        } catch (Exception e) {
+            Log.e(TAG, "Can't remove files from /data/system/* !!! Is root available?", e);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean devAdmCreateDeviceOwner(boolean silent) {
+        if (!remountDataPart(true, silent))
+            return false;
+        String fn = "/data/system/device_owner.xml";
+        String[] cmd = new String[]{
+                "cat > " + fn + " <<\"DEVICE_OWNER1\"",
+                "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>",
+                "<root>",
+                "<device-owner package=\"" + pkgName + "\" name=\"" + appName + "\" />",
+                "</root>",
+                "DEVICE_OWNER1",
+                "chown system:system " + fn
+        };
+        try {
+            doSuCmd(cmd);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to create file " + fn, e);
+            return false;
+        }
+        return true;
+    }
+
     private void onDevAdmInstallButtonClick() {
         if (!remountDataPart(true, false))
             return;
         int x = 0;
         String fn = "";
         try {
-            fn = "/data/system/device_owner.xml";
-            String[] cmd = new String[]{
-                    "cat > " + fn + " <<\"DEVICE_OWNER1\"",
-                    "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>",
-                    "<root>",
-                    "    <device-owner",
-                    "        package=\"" + pkgName + "\"",
-                    "        name=\"" + appName + "\"",
-                    "    />",
-                    "</root>",
-                    "DEVICE_OWNER1",
-                    "chown system:system " + fn
-            };
-            doSuCmd(cmd);
-            x++;
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to create file " + fn, e);
-        }
-        try {
             fn = "/data/system/device_owner_2.xml";
             String[] cmd = new String[]{
                     "cat > " + fn + " <<\"DEVICE_OWNER2\"",
                     "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>",
                     "<root>",
-                    "    <device-owner",
-                    "        package=\"" + pkgName + "\"",
-                    "        name=\"" + appName + "\"",
-                    "        component=\"" + devAdmName.flattenToString() + "\"",
-                    "        userRestrictionsMigrated=\"true\"",
-                    "        canAccessDeviceIds=\"true\"",
-                    "    />",
-                    "    <device-owner-context userId=\"0\" />",
+                    "<device-owner" +
+                    " package=\"" + pkgName + "\"" +
+                    " name=\"" + appName + "\"" +
+                    " component=\"" + devAdmName.flattenToString() + "\"" +
+                    " userRestrictionsMigrated=\"true\"" +
+                    " canAccessDeviceIds=\"true\"" +
+                    " />",
+                    "<device-owner-context userId=\"0\" />",
                     "</root>",
                     "DEVICE_OWNER2",
                     "chown system:system " + fn
@@ -167,12 +192,12 @@ public class SettingsActivity extends AppCompatActivity implements
                     "cat > " + fn + " <<\"DEVICE_POL2\"",
                     "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>",
                     "<root>",
-                    "    <device-owner",
-                    "        package=\"" + pkgName + "\"",
-                    "        name=\"" + appName + "\"",
-                    "        component=\"" + devAdmName.flattenToString() + "\"",
-                    "        userRestrictionsMigrated=\"true\"",
-                    "    />",
+                    "<device-owner" +
+                    " package=\"" + pkgName + "\"" +
+                    " name=\"" + appName + "\"" +
+                    " component=\"" + devAdmName.flattenToString() + "\"" +
+                    " userRestrictionsMigrated=\"true\"" +
+                    " />",
                     "</root>",
                     "DEVICE_POL2",
                     "chown system:system " + fn
@@ -182,7 +207,7 @@ public class SettingsActivity extends AppCompatActivity implements
         } catch (Exception e) {
             Log.e(TAG, "Failed to create file " + fn, e);
         }
-        if (x >= 3)
+        if (x >= 2)
             Toast.makeText(this, "Device-Owner configs installed to /data/system !", Toast.LENGTH_LONG).show();
         else
             Toast.makeText(this, "ERROR: Device-Owner configs NOT installed! " + x, Toast.LENGTH_LONG).show();
@@ -199,17 +224,22 @@ public class SettingsActivity extends AppCompatActivity implements
                     "cat > " + fn + " <<\"DEVICE_POLICIES\"",
                     "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>",
                     "<policies setup-complete=\"true\">",
-                    "    <admin name=\"" + devAdmName.flattenToString() + "\"",
-                    "        <policies flags=\"1023\" />",
-                    "        <strong-auth-unlock-timeout value=\"0\" />",
-                    "        <test-only-admin value=\"true\" />",
-                    "        <user-restrictions no_add_managed_profile=\"true\" />",
-                    "        <default-enabled-user-restrictions>",
-                    "            <restriction value=\"no_add_managed_profile\" />",
-                    "        </default-enabled-user-restrictions>",
-                    "        <cross-profile-calendar-packages />",
-                    "    </admin>",
-                    "    <lock-task-features value=\"16\" />",
+                    //"<admin name=\"com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver\">",
+                    //"<policies flags=\"540\" />",
+                    //"<strong-auth-unlock-timeout value=\"0\" />",
+                    //"</admin>",
+                    "<admin name=\"" + devAdmName.flattenToString() + "\">",
+                    "<policies flags=\"1023\" />",
+                    "<strong-auth-unlock-timeout value=\"0\" />",
+                    "<test-only-admin value=\"true\" />",
+                    "<user-restrictions no_add_managed_profile=\"true\" />",
+                    "<default-enabled-user-restrictions>",
+                    "<restriction value=\"no_add_managed_profile\" />",
+                    "</default-enabled-user-restrictions>",
+                    "<cross-profile-calendar-packages />",
+                    "</admin>",
+                    "<password-validity value=\"true\" />",
+                    "<lock-task-features value=\"16\" />",
                     "</policies>",
                     "DEVICE_POLICIES",
                     "chown system:system " + fn
@@ -256,24 +286,15 @@ public class SettingsActivity extends AppCompatActivity implements
         return true;
     }
 
-    private void onDevAdmDeleteButtonClick(boolean silent) {
+    private void onDevAdmDeleteButtonClick() {
         if (!remountDataPart(true, false))
             return;
-        String[] cmd = new String[]{
-                "rm -f /data/system/device_owner.xml",
-                "rm -f /data/system/device_owner_2.xml",
-                "rm -f /data/system/device_policy_2.xml",
-                "rm -f /data/system/device_policies.xml"
-        };
-        try {
-            doSuCmd(cmd);
-            if (!silent)
-                Toast.makeText(this, "All XML files deleted! Please, REBOOT device!", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            Log.e(TAG, "Can't remove files from /data/system/* !!! Is root available?", e);
-            if (!silent)
-                Toast.makeText(this, "ERROR: XML files NOT deleted!", Toast.LENGTH_LONG).show();
-        }
+        String flist = "dev_owner + dev_owner_2 + dev_pol_2 + dev_pol";
+        boolean x = devAdmDeleteFiles(flist, false);
+        if (x)
+            Toast.makeText(this, "All XML files deleted! Please, REBOOT device!", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "ERROR: XML files NOT deleted!", Toast.LENGTH_LONG).show();
     }
 
     public void doSuCmd(String[] commands) throws Exception {
